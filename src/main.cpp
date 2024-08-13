cb6495c2bd1d7ab2ea35007be60905f20b3b9150
John Heikens <real.johnheikens@gmail.com>
2024-08-13 17:23:38 +0200

    add code

--- src/main.cpp
+++ src/main.cpp
@@ -3,19 +3,126 @@
 #include "include/application/application.h"
 #include "gameInfo.h"
 #include "math/random/random.h"
+#include "soundStream.h"
+#include "include/math/algorithm/iterate2Dto1D.h"
+#include "include/math/swingsynchronizer.h"
+#include "include/math/sound/sound.h"
+
+enum tileType
+{
+    air,
+    stone,
+    danger
+};
+
+struct tile
+{
+    tileType type = air;
+};
+constexpr int size = 0x400;
+constexpr int area = size * size;
+array2d<tile> tiles = array2d<tile>(veci2(size));
+veci2 positions[area]{};
+
+//constexpr fp a4Hz = 440;
+//
+//constexpr fp a5Hz = a4Hz * 2;
+//
+//const swingSynchronizer stoneNote = swingSynchronizer(sampleRate / a4Hz, soundMin, soundMax);
+//const swingSynchronizer dangerNote = swingSynchronizer(sampleRate / a5Hz, soundMin, soundMax);
+
+fp lowestNote = log2(minObservableHZ);
+fp highestNote = log2(maxObservableHZ);
+
+class audializer : public rawSoundStream
+{
+public:
+    texture imageToAudialize;
+    /// @brief the audializer will loop over this picture and convert it to sound!
+    /// @param imageToAudialize will make a copy!
+    audializer(const texture &imageToAudialize) : imageToAudialize(imageToAudialize)
+    {
+    }
+
+protected:
+    size_t iterationIndex = 0;
+    // amount of samples this audializer played since its construction
+    int samplesPlayed = 0;
+    swingSynchronizer sineSynchronizer = swingSynchronizer(0, soundMin, soundMax);
+    virtual bool onGetData(Chunk &data)
+    {
+        if (rawSoundStream::onGetData(data))
+        {
+            std::fill(samples.begin(), samples.end(), 0);
+
+            for (int bufferIndex = 0; bufferIndex < audioBufSize; iterationIndex++, samplesPlayed++)
+            {
+                size_t iterationIndex = (samplesPlayed * (size_t)area) / sampleRate;
+                iterationIndex %= area;
+                const color currentColor = imageToAudialize.getValue(positions[iterationIndex]);
+                const colorf hsvColor = rgb2hsv(currentColor);
+                short amp;
+                // higher pitch = lighter color
+
+                sineSynchronizer.swingDuration = pow(2, math::lerp(lowestNote, highestNote, hsvColor.v()));
+                amp = (short)sineSynchronizer.getSwingAngle((fp)samplesPlayed);
+                // if (currentTile.type == stone)
+                //{
+                //     amp = (short)stoneNote.getSwingAngle((fp)samplesPlayed);
+                // }
+                // else if (currentTile.type == danger)
+                //{
+                //     amp = (short)dangerNote.getSwingAngle((fp)samplesPlayed);
+                // }
+                // else
+                //     amp = rand(currentRandom, soundMin, soundMax);
+                samples[bufferIndex++] = amp;
+                samples[bufferIndex++] = amp;
+            }
+            return true;
+        }
+        else
+            return false;
+    }
+};
+
 struct gameForm : public form
 {
+    gameForm()
+    {
+        size_t index = 0;
+        const auto l = [this, &index](cveci2 &pos)
+        {
+            positions[index++] = pos;
+        };
+        iterate2Dto1D(veci2(), size, directionID::positiveX, l);
+        for (tile &t : tiles)
+        {
+            t.type = (tileType)randIndex(currentRandom, 3);
+        }
+    }
     virtual void render(cveci2 &position, const texture &renderTarget) override
     {
-        for(color& ptr : renderTarget){
-            ptr = color((byte)rand(0x100), (byte)rand(0x100), (byte)rand(0x100));
+        fp hue = 0;
+        cfp &step = 360.0 / area;
+        for (const veci2 &pos : positions)
+        {
+            const color &val = (tiles.getValue(pos).type == air) ? (color)hsv2rgb(colorf(hue += step, 1, 1)) : colorPalette::white;
+            renderTarget.setValue(pos, val);
         }
-        //renderTarget.fill(color((byte)rand(0x100), (byte)rand(0x100), (byte)rand(0x100)));
+        // for(color& ptr : renderTarget){
+        //     ptr = color((byte)rand(0x100), (byte)rand(0x100), (byte)rand(0x100));
+        // }
+        // renderTarget.fill(color((byte)rand(0x100), (byte)rand(0x100), (byte)rand(0x100)));
     }
 };
 gameForm *mainForm = new gameForm();
 int main(int argc, char *argv[])
 {
+    const texture &tex = texture(L"data/images/biker.jpg", true);
+    audializer stream(tex);
+    stream.SetBufSize(audioBufSize, channelCount, sampleRate);
+    stream.play();
     // execute this function before you do anything,
     initialize();
     return application(mainForm, gameName).run();
---
+++ src/soundStream.h
@@ -0,0 +1,49 @@
+#include <SFML/Audio.hpp>
+#include "math/random/random.h"
+
+// the amount of channels
+constexpr int channelCount = 2;
+
+constexpr int samplesPerChunk = 0x400;
+
+// the size of the audio buffer.
+constexpr int audioBufSize = samplesPerChunk * channelCount;
+
+// inclusive max sample amplitudes
+constexpr short soundMax = 32767, soundMin = -32768;
+
+constexpr int sampleRate = 44100;
+// original:
+// https://en.sfml-dev.org/forums/index.php?topic=24924.0
+
+auto currentRandom = getGeneratorBasedOnTime();
+
+class rawSoundStream : public sf::SoundStream
+{
+public:
+    void SetBufSize(int sz, int channelCount, int sampleRate)
+    {
+        samples.resize(sz, 0);
+        currentSample = 0;
+        initialize(channelCount, sampleRate);
+    }
+
+protected:
+    virtual bool onGetData(Chunk &data)
+    {
+        data.samples = &samples[currentSample];
+
+        data.sampleCount = audioBufSize;
+        currentSample = 0;
+
+        return true;
+    }
+
+    virtual void onSeek(sf::Time timeOffset)
+    {
+        currentSample = static_cast<std::size_t>(timeOffset.asSeconds() * (float)getSampleRate() * (float)getChannelCount());
+    }
+
+    std::vector<sf::Int16> samples;
+    std::size_t currentSample;
+};
